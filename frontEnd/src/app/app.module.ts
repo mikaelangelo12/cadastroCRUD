0ddf5f20e5e68a2a242b134cc680bffdc848ac12
Mikael <mikael@mikaelangelo.meuintelbras.local>
2021-11-17 02:50:32 -0300

    Criando header e atribuindo nome Aplicação GRUD

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/templete/header/header.component';
+import { FooterComponent } from './components/templete/footer/footer.component';
+import { NavComponent } from './components/templete/nav/nav.component';
+import { ThemesMatModule } from 'themesMat';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
+    ThemesMatModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
