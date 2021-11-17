8024b37a926b63ed1b6f6a91871ab90f3e0d3080
Mikael <mikael@mikaelangelo.meuintelbras.local>
2021-11-17 08:01:31 -0300

    Criando Pagina Home

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { HeaderComponent } from './components/templete/header/header.component';
 import { FooterComponent } from './components/templete/footer/footer.component';
 import { NavComponent } from './components/templete/nav/nav.component';
 import { ThemesMatModule } from 'themesMat';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
